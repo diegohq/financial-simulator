f44e071309b3f62b1f8594ea2629a8cc6d27da3a
Diego Quintale <diegohquintale@gmail.com>
2022-06-09 14:12:50 -0300

    Ads

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    @if(App::environment('production'))
+    @if(App::environment(['production']))
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-85ZMZMW04P"></script>
     <script>
@@ -30,17 +30,54 @@
     <div class="container">
         <div class="row mt-3">
 
-            <div class="col-md-3"><!-- AD --></div>
+            <div class="col-md-3">
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5507757504814188"
+                        crossorigin="anonymous"></script>
+                <!-- Laterais -->
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-5507757504814188"
+                     data-ad-slot="8971283441"
+                     data-ad-format="auto"
+                     data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
             <div class="col-md-6 col-12">
                 <h1 class="mb-3">Simuladores de investimento</h1>
                 <div id="simulator"></div>
             </div>
-            <div class="col-md-3"><!-- AD --></div>
+            <div class="col-md-3">
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5507757504814188"
+                        crossorigin="anonymous"></script>
+                <!-- Laterais -->
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-5507757504814188"
+                     data-ad-slot="8971283441"
+                     data-ad-format="auto"
+                     data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
 
         </div>
 
-        <div class="mt-3">
-
+        <div class="row mt-3">
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5507757504814188"
+                    crossorigin="anonymous"></script>
+            <!-- Inferior -->
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-5507757504814188"
+                 data-ad-slot="2292262824"
+                 data-ad-format="auto"
+                 data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
         </div>
 
     </div>
